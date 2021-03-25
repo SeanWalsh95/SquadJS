6192a75b5df43ac580698b5707c0c0081db8f5a3
SeanWalsh95 <SeanWalsh95@users.noreply.github.com>
2021-03-25 13:52:56 -0400

    adjust formatting

--- squad-server/plugins/discord-seeding-rewards.js
+++ squad-server/plugins/discord-seeding-rewards.js
@@ -195,10 +195,11 @@ export default class DiscordSeedingRewards extends DiscordBasePlugin {
     const m = Math.floor((seconds % 3600) / 60);
     const s = Math.floor(seconds % 60);
 
-    const dDisplay = d > 0 ? d + (d === 1 ? ' day, ' : ' days, ') : '';
-    const hDisplay = h > 0 ? h + (h === 1 ? ' hour, ' : ' hours, ') : '';
-    const mDisplay = m > 0 ? m + (m === 1 ? ' minute, ' : ' minutes, ') : '';
-    const sDisplay = s > 0 ? s + (s === 1 ? ' second' : ' seconds') : '';
-    return dDisplay + hDisplay + mDisplay + sDisplay;
+    const display = [];
+    display.push(d > 0 ? d + (d === 1 ? ' day' : ' days') : '');
+    display.push(h > 0 ? h + (h === 1 ? ' hour' : ' hours') : '');
+    display.push(m > 0 ? m + (m === 1 ? ' minute' : ' minutes') : '');
+    display.push(s > 0 ? s + (s === 1 ? ' second' : ' seconds') : '');
+    return display.join(', ');
   }
 }

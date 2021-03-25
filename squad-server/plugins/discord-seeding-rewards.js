437c185e1d89003e77bf73cf8a9a1f7ac45244f0
SeanWalsh95 <SeanWalsh95@users.noreply.github.com>
2021-03-25 13:47:17 -0400

    update format seconds

--- squad-server/plugins/discord-seeding-rewards.js
+++ squad-server/plugins/discord-seeding-rewards.js
@@ -189,14 +189,16 @@ export default class DiscordSeedingRewards extends DiscordBasePlugin {
     if (expired.length > 0) this.verbose(1, `${expired.length} rewards removed...`);
   }
 
-  formatSeconds(timeInSeconds) {
-    // take in generic # of seconds and return formatted HH:MM
-    const hr = Math.floor((timeInSeconds / 3600) % 24);
-    let min = Math.floor((timeInSeconds / 60) % 60);
-    // let sec = Math.floor(timeInSeconds % 60);
+  formatSeconds(seconds) {
+    const d = Math.floor(seconds / (3600 * 24));
+    const h = Math.floor((seconds % (3600 * 24)) / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = Math.floor(seconds % 60);
 
-    min = `${min}`.padStart(2, '0');
-    // sec = (`${sec}`).padStart(2, '0');
-    return `${hr} hours and ${min} minutes`;
+    const dDisplay = d > 0 ? d + (d === 1 ? ' day, ' : ' days, ') : '';
+    const hDisplay = h > 0 ? h + (h === 1 ? ' hour, ' : ' hours, ') : '';
+    const mDisplay = m > 0 ? m + (m === 1 ? ' minute, ' : ' minutes, ') : '';
+    const sDisplay = s > 0 ? s + (s === 1 ? ' second' : ' seconds') : '';
+    return dDisplay + hDisplay + mDisplay + sDisplay;
   }
 }
